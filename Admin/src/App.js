9996fa3013e303602df8f40482b255985694cb24
Jin hyeon <kangjin627@gmail.com>
2023-07-04 13:51:56 +0900

    [ADD] 프로젝트 세팅

---
+++ Admin/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      hihi
+    </div>
+  );
+}
+
+export default App;
---
+++ User/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      byebye
+    </div>
+  );
+}
+
+export default App;
